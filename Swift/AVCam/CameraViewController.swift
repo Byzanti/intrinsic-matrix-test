2462e11f252eaaa2f227f6ab3959c775229706f7
Peter <peter@byzanti.co.uk>
2020-02-09 21:03:35 +0000

    App overwritten to get intrinsic matrix

---
+++ Swift/AVCam/CameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewController.swift
+//  AVCam
+//
+//  Created by Peter on 08/02/2020.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
